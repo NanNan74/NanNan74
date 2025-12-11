f6d32eccb6ade71d1f012743cf2f9ff5e405e0e8
NanNan74 <nhuclaus74@gmail.com>
2025-12-11 20:44:04 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
 import { Device, SystemConfig } from './types';
 
 export const DEFAULT_DEVICES: Device[] = [
-  { id: 'tank-001', name: 'Device tank-001 (Bể chứa A)', location: 'Zone A' },
-  { id: 'river-002', name: 'Device river-002 (Sông Hương)', location: 'Zone B' },
-  { id: 'sewer-003', name: 'Device sewer-003 (Cống thoát 3)', location: 'Zone C' },
+  { id: 'tank-001', name: 'Nhà Ánh Như', location: 'Zone A' },
+  { id: 'river-002', name: 'Nhà Ngọc Anh)', location: 'Zone B' },
+  { id: 'sewer-003', name: 'Nhà Thủy Tiên', location: 'Zone C' },
 ];
 
 export const DEFAULT_CONFIG: SystemConfig = {
