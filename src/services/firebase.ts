70f2bf44368da29e9dcfbedc5c2fe89e2e4d6416
NanNan74 <nhuclaus74@gmail.com>
2025-12-12 17:40:47 +0700

    Create firebase.ts

---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4paYQZK5ihS0dbVrPIWFEpXdfwLjZzVw",
+  authDomain: "canhbaolulut-bcdf0.firebaseapp.com",
+  databaseURL: "https://canhbaolulut-bcdf0-default-rtdb.firebaseio.com",
+  projectId: "canhbaolulut-bcdf0",
+  storageBucket: "canhbaolulut-bcdf0.firebasestorage.app",
+  messagingSenderId: "963271211348",
+  appId: "1:963271211348:web:278cadf7886d03ab0f30c1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
