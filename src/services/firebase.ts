763b78569fa0558ab71d46275aae87fd973ccc1e
NanNan74 <nhuclaus74@gmail.com>
2025-12-12 18:49:49 +0700

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,7 +4,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyD4paYQZK5ihS0dbVrPIWFEpXdfwLjZzVw",
   authDomain: "canhbaolulut-bcdf0.firebaseapp.com",
-  databaseURL: "https://canhbaolulut-bcdf0-default-rtdb.firebaseio.com",
+  databaseURL: "https://canhbaolulut-bcdf0-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "canhbaolulut-bcdf0",
   storageBucket: "canhbaolulut-bcdf0.firebasestorage.app",
   messagingSenderId: "963271211348",
